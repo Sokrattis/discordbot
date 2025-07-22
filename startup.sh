2595dfe6ce7ea430b7c74441fe291cbc08621a7a
Sokrattis <piolimonteiro.fabio@gmail.com>
2025-07-22 19:38:01 -0300

    ultima tentativa

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/site/repository
-echo "Iniciando bot manualmente via startup.sh..."
+cd /home/site/wwwroot
+echo "🔧 Iniciando health‑check HTTP na porta 8000"
+nohup python3 -m http.server 8000 >/dev/null 2>&1 &
+echo "🔧 Iniciando Discord‑Bot"
 python3 authades.py
